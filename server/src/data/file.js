03a75ea95439d0e177227eab0063994d80cd840f
Christoph Brand <christoph@brand.rest>
2015-05-27 00:49:28 +0200

    server: fixed some issues in the current file implementation.

--- server/src/data/file.js
+++ server/src/data/file.js
@@ -1,6 +1,7 @@
 
 var fs = require('fs');
 var constants = require('../constants');
+var q = require('q');
 
 class File {
     constructor(path) {
@@ -24,11 +25,11 @@ class File {
     }
 
     /**
-     * Reads a chunk from the given file and returns the bytes represented
-     * by the given chunk.
+     * chunkStream returns the stream with the passed num data.
      * @param {number} num
+     * @returns {fs.ReadStream} The opened read stream.
      */
-    chunk(num) {
+    chunkStream(num) {
         var numChunks = this.numChunks;
         if(numChunks <= num) {
             throw new Error('Requested chunk number to high. Only have ' + numChunks + ' chunks.');
@@ -36,14 +37,41 @@ class File {
             throw new Error('number must be positive.');
         }
         var stat = this._stat;
+        return fs.createReadStream(
+            this.path,
+            {
+                start: constants.CHUNK_SIZE * num,
+                end: Math.min(
+                    constants.CHUNK_SIZE * (num+1),
+                    stat.size
+                ) - 1
+            }
+        );
+    }
 
-        return fs.createReadStream(this.path, {
-            start: constants.CHUNK_SIZE * numChunks,
-            end: Math.min(
-                constants.CHUNK_SIZE * (numChunks+1),
-                stat.size
-            )
+    /**
+     * Reads a chunk from the given file and returns the bytes represented
+     * by the given chunk.
+     * @param {number} num
+     * @returns {Promise} will resolved as soon as the data is available.
+     */
+    chunk(num) {
+        var deferred = q.defer(),
+            readStream = this.chunkStream(num),
+            data = ""
+        ;
+
+        readStream.on('data', function(chunk) {
+            data += chunk;
         });
+        readStream.on('error', function(err) {
+            deferred.reject(err);
+        });
+        readStream.on('end', function() {
+            deferred.resolve(data);
+        });
+
+        return deferred.promise;
     }
 }
 
