1ca6edd9f6cdf6198d91c1ef1d02893cc5a955aa
Christoph Brand <christoph@brand.rest>
2015-06-01 01:48:34 +0200

    server: added tests to verify the correct file creation and definition of the file object.

--- server/spec/handlers/fileSpec.js
+++ server/spec/handlers/fileSpec.js
@@ -39,19 +39,81 @@ describe ('FileHandler', function() {
         temp.cleanupSync();
     });
 
-    it('should be able to add a file', function(done) {
+    var sendReadableStream = function() {
         var s = new stream.Readable();
         s._read = function noop() {};
+        var args = arguments;
+        setImmediate(function() {
+            for(var argument in args) {
+                s.push(argument);
+            }
+            s.push(null);
+        });
+        return s;
+    };
 
+    it('should be able to add a file', function(done) {
         var fileHandler = new FileHandler(config);
-        fileHandler.add('test.txt', s, {}).then(function(uuid) {
-            expect(uuid).not.toBeFalsy();
+        var stream = sendReadableStream('Testfile', 'More stuff');
+        fileHandler.add('test.txt', stream, {}).then(function(obj) {
+            expect(obj).not.toBeFalsy();
             done();
         });
+    });
 
-        s.push('Testfile');
-        s.push('More stuff');
-        s.push(null);
-    }, 250);
+    var addData = function(options) {
+        options = options || {};
+        var fileHandler = new FileHandler(config);
+        var stream = sendReadableStream('Testfile', 'More stuff', 'And much more stuff');
+        return fileHandler.add(options.fileName || 'test2.txt', stream, options);
+    };
+
+    it('should return a uuid.', function(done) {
+        addData().then(function(obj) {
+            expect(obj.uuid).toBeTruthy();
+        }).then(done);
+    });
+
+    it('should return the correct filename', function(done) {
+        addData({fileName: 'test.txt'}).then(function(obj) {
+            expect(obj.fileName).toEqual('test.txt');
+        }).then(done);
+    });
+
+    it('should automatically set the correct mimetype (for text)', function(done) {
+        addData({fileName: 'test.txt'}).then(function(obj) {
+            expect(obj.mimeType).toEqual('text/plain');
+        }).then(done);
+    });
+
+    it('should automatically set the correct mimetype (for pdf)', function(done) {
+        addData({fileName: 'test.pdf'}).then(function(obj) {
+            expect(obj.mimeType).toEqual('application/pdf');
+        }).then(done);
+    });
+
+    it('should be able to manually overwrite the mimetype', function(done) {
+        addData({fileName: 'test.pdf', mimeType: 'text/plain'}).then(function(obj) {
+            expect(obj.mimeType).toEqual('text/plain');
+        }).then(done);
+    });
+
+    it('should create a file', function(done) {
+        addData({fileName: 'test.pdf', mimeType: 'text/plain'}).then(function(obj) {
+            var uuidPath = path.join(fileDirectory, obj.uuid);
+            fs.exists(uuidPath, function(exists) {
+                expect(exists).toBeTruthy();
+            });
+        }).then(done);
+    });
+
+    it('should have the correct file contents', function(done) {
+        addData({fileName: 'test.pdf', mimeType: 'text/plain'}).then(function(obj) {
+            var uuidPath = path.join(fileDirectory, obj.uuid);
+            fs.readFile(uuidPath, function(data) {
+                expect(data).toEqual('TestfileMore StuffAnd much more stuff');
+            });
+        }).then(done);
+    });
 
 });
