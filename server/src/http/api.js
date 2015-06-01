0174e48fad8282fd1e5c5766d5d610ec09018c20
Christoph Brand <christoph@brand.rest>
2015-06-01 23:11:02 +0200

    server: refactored api to load fileUUID automatically if it is requested as a param utilizing the express param function.

--- server/src/http/api.js
+++ server/src/http/api.js
@@ -3,26 +3,33 @@ var HttpStatus = require('http-status-codes');
 
 var api = express.Router();
 
-api.get('/files/:uuid', function(req, res) {
+api.param('fileUUID', function(req, res, next, uuid) {
     var fileHandler = req.app.get('fileHandler');
-
-    fileHandler.get(req.params.uuid).then(function(file) {
-        res.status(HttpStatus.OK).send({
-            uuid: file.uuid,
-            fileName: file.fileName,
-            mediaType: file.mediaType,
-            chunks: file.numChunks
-        });
+    fileHandler.get(uuid).then(function(file) {
+        req.file = file;
+        next();
     }).catch(function(err) {
+        /* istanbul ignore else: This should never happen */
         if(err && err.isNotExist) {
             res.status(HttpStatus.NOT_FOUND).send({
                 error: HttpStatus.getStatusText(HttpStatus.NOT_FOUND)
             });
-            return;
+        } else {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+                error: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
+            });
         }
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
-            error: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR)
-        });
+        next(new Error('Not found'));
+    });
+});
+
+api.get('/files/:fileUUID', function(req, res) {
+    var file = req.file;
+    res.status(HttpStatus.OK).send({
+        uuid: file.uuid,
+        fileName: file.fileName,
+        mediaType: file.mediaType,
+        chunks: file.numChunks
     });
 });
 
