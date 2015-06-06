7d3459798805fac78c50c7abd673b87e4c669cfd
Christoph Brand <christoph@brand.rest>
2015-06-06 17:14:55 +0200

    server: added test to check that non p2pcdn channels are not accepted.

--- server/test/rtc/connectionSpec.js
+++ server/test/rtc/connectionSpec.js
@@ -5,9 +5,10 @@ var helpers = require('../helpers');
 var Connection = helpers.require('rtc/connection');
 
 describe('Connection', function () {
-    var connection;
+    var serverConnection;
+    var clientConnection;
 
-    var initConnection = function (serverConnection, clientConnection) {
+    var initConnection = function () {
         return Q.Promise(function (resolve, reject) {
             clientConnection.createOffer(function (clientDescriptor) {
                 resolve(clientDescriptor);
@@ -36,34 +37,81 @@ describe('Connection', function () {
     };
 
     it('should be able to open a channel connection', function () {
-        connection = new Connection();
-        connection.start();
+        serverConnection = new Connection();
+        serverConnection.start();
 
-        var clientConnection = new wrtc.RTCPeerConnection();
+        clientConnection = new wrtc.RTCPeerConnection();
         clientConnection.onicecandidate = function (candidate) {
             if (!candidate.candidate) {
                 return;
             }
-            connection.addIceCandidate(candidate.candidate);
+            serverConnection.addIceCandidate(candidate.candidate);
         };
-        connection.on('icecandidate', function (candidate) {
+        serverConnection.on('icecandidate', function (candidate) {
             clientConnection.addIceCandidate(candidate);
         });
         var clientChannel = clientConnection.createDataChannel('p2pcdn');
 
-        return initConnection(connection, clientConnection).then(function () {
-            return Q.Promise(function(resolve, reject) {
+        return initConnection().then(function () {
+            return Q.Promise(function (resolve, reject) {
                 clientChannel.onopen = function () {
                     resolve();
                 };
-                clientChannel.onerror = function(err) {
+                clientChannel.onerror = function (err) {
                     reject(err);
                 }
             });
         }).then(function () {
-            connection.close();
+            serverConnection.close();
         }).then(function () {
             clientConnection.close();
         });
     });
-});
+
+    describe('when connected', function () {
+        var clientChannel;
+
+        beforeEach(function () {
+            serverConnection = new Connection();
+            serverConnection.start();
+
+            clientConnection = new wrtc.RTCPeerConnection();
+            clientConnection.onicecandidate = function (candidate) {
+                if (!candidate.candidate) {
+                    return;
+                }
+                serverConnection.addIceCandidate(candidate.candidate);
+            };
+            serverConnection.on('icecandidate', function (candidate) {
+                clientConnection.addIceCandidate(candidate);
+            });
+            clientChannel = clientConnection.createDataChannel('p2pcdn');
+            return initConnection();
+        });
+
+        it('should reject data channels which aren\'t marked as p2pcdn', function () {
+            var otherChannel = clientConnection.createDataChannel('notp2pcnd');
+
+            var createEventPromise = function(name) {
+                return Q.Promise(function(resolve, reject) {
+                    otherChannel.addEventListener(name, function() {
+                        resolve();
+                    });
+                    otherChannel.addEventListener('error', function() {
+                        reject();
+                    });
+                });
+            };
+
+            return createEventPromise('open').then(function() {
+                return createEventPromise('close');
+            })
+        });
+
+        afterEach(function () {
+            serverConnection.close();
+            clientConnection.close();
+        });
+    })
+})
+;
