1c09ae33f067533d58ea1c1664800677747d7df9
Christoph Brand <christoph@brand.rest>
2015-07-02 23:26:53 +0200

    common: made ClientNegotiation publicly available through the message interface.

--- src/common/messages/message.js
+++ src/common/messages/message.js
@@ -1,20 +1,18 @@
 
 var Message = require('./message/message');
 var Chunk = require('./message/chunk');
-var ClientNegotiation = require('./message/client_negotiation');
-var ErrorResponse = require('./message/error');
-var Init = require('./message/init');
+var Error = require('./message/error');
 var FileInfo = require('./message/file_info');
 var GetChunk = require('./message/get_chunk');
 var GetFileInfo = require('./message/get_file_info');
+var ClientNegotiation = require('./message/client_negotiation');
 
 export {
     Message,
     Chunk,
     ClientNegotiation,
-    ErrorResponse as Error,
+    Error,
     FileInfo,
     GetChunk,
-    GetFileInfo,
-    Init
+    GetFileInfo
 };
