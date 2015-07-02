292254ab6cdc79efe6c033e2ad22bfb68f5386b8
Christoph Brand <christoph@brand.rest>
2015-07-03 00:45:04 +0200

    common: readded import from init accidentally being removed in a former commit.

--- src/common/messages/message.js
+++ src/common/messages/message.js
@@ -1,18 +1,20 @@
 
 var Message = require('./message/message');
 var Chunk = require('./message/chunk');
-var Error = require('./message/error');
+var ClientNegotiation = require('./message/client_negotiation');
+var ResponseError = require('./message/error');
 var FileInfo = require('./message/file_info');
 var GetChunk = require('./message/get_chunk');
 var GetFileInfo = require('./message/get_file_info');
-var ClientNegotiation = require('./message/client_negotiation');
+var Init = require('./message/init');
 
 export {
     Message,
     Chunk,
     ClientNegotiation,
-    Error,
+    ResponseError as Error,
     FileInfo,
     GetChunk,
-    GetFileInfo
+    GetFileInfo,
+    Init
 };
