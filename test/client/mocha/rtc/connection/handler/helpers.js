3387a4e545b59ccc7de54a8e5f9b67d5ff73a272
Christoph Brand <christoph@brand.rest>
2015-07-03 02:51:08 +0200

    test/server: added test to check if the rtc connection establishing works for both the caller and callee side.

---
+++ test/client/mocha/rtc/connection/handler/helpers.js
@@ -0,0 +1,31 @@
+var helpers = require('../../../helpers');
+var messages = helpers.require('messages/message');
+
+var RTCChannelMock = function () {
+
+};
+
+RTCChannelMock.prototype.connect = function (serverRTCChannel) {
+    var self = this;
+    self.remoteChannel = serverRTCChannel;
+};
+
+RTCChannelMock.prototype.send = function (data) {
+    var self = this;
+    setTimeout(self.remoteChannel.onmessage.bind(
+        self.remoteChannel,
+        {
+            data: data
+        }
+    ), 0);
+};
+
+RTCChannelMock.prototype.close = function () {
+    var self = this;
+    if (self.onclose) {
+        setTimeout(self.onclose.bind(self), 0);
+    }
+};
+
+
+module.exports.RTCChannelMock = RTCChannelMock;
---
+++ test/client/mocha/rtc/connection/handler/throughRTCSpec.js
@@ -0,0 +1,78 @@
+var Q = require('q');
+var helpers = require('../../../helpers');
+helpers.emulateBrowser();
+
+var rtcHelpers = require('./helpers');
+var RTCChannelMock = rtcHelpers.RTCChannelMock;
+
+var RTCChannelHandler = helpers.require('rtc/channel_handler');
+var WSConnectionHandler = helpers.require('rtc/connection/handler/serverWs');
+var RTCCallerConnectionHandler = helpers.require('rtc/connection/handler/throughRTC/caller');
+var RTCCalleeConnectionHandler = helpers.require('rtc/connection/handler/throughRTC/callee');
+
+
+describe('Connection', function () {
+
+    describe('throughRTCHandler', function () {
+        var calleeRTCChannelMock;
+        var callerRTCChannelMock;
+
+        var rtcCalleeChannelHandler;
+        var rtcCallerChannelHandler;
+
+        beforeEach(function() {
+            callerRTCChannelMock = new RTCChannelMock();
+            calleeRTCChannelMock = new RTCChannelMock();
+            calleeRTCChannelMock.connect(callerRTCChannelMock);
+            callerRTCChannelMock.connect(calleeRTCChannelMock);
+
+            rtcCalleeChannelHandler = new RTCChannelHandler(calleeRTCChannelMock, {
+                rtc: true
+            });
+            rtcCallerChannelHandler = new RTCChannelHandler(callerRTCChannelMock, {
+                rtc: true
+            });
+        });
+
+        it('should be able to connect two clients together', function() {
+            var negotiationId = 'this-is-just-random-data';
+
+            var rtcCallerConnectionHandler = new RTCCallerConnectionHandler(
+                rtcCallerChannelHandler,
+                negotiationId);
+
+            var rtcCalleeConnectionHandler;
+
+            var promises = [];
+
+            return new Q.Promise(function(resolve, reject) {
+                rtcCalleeChannelHandler.once('new_rtc_offer', function(data) {
+                    try {
+                        rtcCalleeConnectionHandler = new RTCCalleeConnectionHandler(
+                            rtcCalleeChannelHandler,
+                            data.negotiationId,
+                            data.offer
+                        );
+                        promises.push(rtcCalleeConnectionHandler.connect());
+                        resolve();
+                    } catch(err) {
+                        reject(err);
+                    }
+                });
+                promises.push(rtcCallerConnectionHandler.connect());
+            }).then(function() {
+                    return Q.all(promises);
+                }).then(function(channels) {
+                    channels.forEach(function(channel) {
+                        channel.close();
+                    });
+                }).then(function() {
+                    rtcCallerConnectionHandler.connection.close();
+                    rtcCalleeConnectionHandler.connection.close();
+                });
+
+        });
+
+    });
+
+});
